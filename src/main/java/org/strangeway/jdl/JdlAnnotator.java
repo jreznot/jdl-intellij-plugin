34d67a8971b935333dc1ba385fc85249be660373
Yuriy Artamonov <jreznot@gmail.com>
2022-08-13 20:18:20 +0200

    Better README

--- src/main/java/org/strangeway/jdl/JdlAnnotator.java
+++ src/main/java/org/strangeway/jdl/JdlAnnotator.java
@@ -4,6 +4,7 @@ import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.strangeway.jdl.model.JdlEnumListType;
 import org.strangeway.jdl.model.JdlEnumType;
@@ -44,13 +45,11 @@ final class JdlAnnotator implements Annotator {
               .range(element.getTextRange())
               .textAttributes(JdlSyntaxHighlighter.JDL_CONSTANT)
               .create();
-        } else if (idParent.getParent() instanceof JdlOptionNameValue) {
-          JdlOptionNameValue optionNameValue = (JdlOptionNameValue) idParent.getParent();
-          annotateOptionNameEnumValue(element, holder, optionNameValue);
-        } else if (idParent.getParent() instanceof JdlArrayLiteral
-            && idParent.getParent().getParent() instanceof JdlOptionNameValue) {
-          JdlOptionNameValue optionNameValue = (JdlOptionNameValue) idParent.getParent().getParent();
-          annotateOptionNameEnumValue(element, holder, optionNameValue);
+        } else {
+          PsiElement optionNameValue = PsiTreeUtil.findFirstParent(idParent, p -> p instanceof JdlOptionNameValue);
+          if (optionNameValue instanceof JdlOptionNameValue) {
+            annotateOptionNameEnumValue(element, holder, (JdlOptionNameValue) optionNameValue);
+          }
         }
       }
     } else if (element instanceof JdlRelationshipType) {
